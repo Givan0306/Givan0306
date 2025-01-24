b6b1a404c97bae7cc26a24d758b057601617f975
Givan0306 <95604140+Givan0306@users.noreply.github.com>
2025-01-24 20:46:59 -0300

    alt

--- app.js
+++ app.js
@@ -21,8 +21,11 @@ while (chute != numeroSecreto) {
     }
 }
 
-if (tentativas > 1) {
-    alert(`Isso ai vc descobriu numero secreto ${numeroSecreto} com ${tentativas} tentativas`);
-} else {
-    alert(`Isso ai vc descobriu numero secreto ${numeroSecreto} com ${tentativas} tentativa`);
-}
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Isso ai vc descobriu numero secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
+//if (tentativas > 1) {
+//    alert(`Isso ai vc descobriu numero secreto ${numeroSecreto} com ${tentativas} tentativas`);
+//} else {
+//    alert(`Isso ai vc descobriu numero secreto ${numeroSecreto} com ${tentativas} tentativa`);
+//}
