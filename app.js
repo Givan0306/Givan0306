37b64aa02f4b8e24b9872f8ae07133b21eb09ba5
Givan0306 <95604140+Givan0306@users.noreply.github.com>
2025-01-24 20:13:00 -0300

    alt

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 alert("Boas vindas ao jogo do numero secreto!");
 let numeroSecreto = 5;
 console.log(numeroSecreto)
-let chute = prompt("Escolha um número entre 1 e 10");
+let chute 
 
-// se o chute for igual ao numero secreto
-if (chute == numeroSecreto) {
-    alert(`Isso ai vc descobriu numero secreto ${numeroSecreto}`);
-} else {
-    if (chute > numeroSecreto){
-        alert(`O numero é menor que ${chute}`);
+//enquanto chute não fo igual ao n. secreto
+while (chute != numeroSecreto) {
+    chute = prompt("Escolha um numero entre 1 e 10");
+    // se o chute for igual ao numero secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso ai vc descobriu numero secreto ${numeroSecreto}`);
     } else {
-        alert(`O numero é maior que ${chute}`);
+        if (chute > numeroSecreto) {
+            alert(`O numero é menor que ${chute}`);
+        } else {
+            alert(`O numero é maior que ${chute}`);
+        }
     }
-        
 }
